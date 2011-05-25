d00d165f90c020445fcb54a7d95633a730b9e075
Federico Mon <gnufede@gmail.com>
2011-05-25 15:22:10 +0000

    Layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Return a title on a per-page basis.               # Documentation comment
+  def title                                           # Method definition
+    base_title = "Ninjas Team"                        # Variable assignment
+    if @title.nil?                                    # Boolean test for nil
+      base_title                                      # Implicit return
+    else
+      "#{base_title} | #{@title}"                     # String interpolation
+    end
+  end
 end
